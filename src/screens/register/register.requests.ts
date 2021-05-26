137f64790f49e5366f56047c2f0a64143955cbbe
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2021-05-26 13:32:31 -0300

    tweaks

--- src/screens/register/register.requests.ts
+++ src/screens/register/register.requests.ts
@@ -3,6 +3,7 @@ const axios = require("axios").default;
 export const register = ({ username, password, mail, name, lastname }) =>
   axios({
     method: "post",
-    url: "http://localhost:8080/user/save",
+    url: "http://localhost:9000/user/register",
+    haders: { "Content-Type": "application/json" },
     data: { username, password, mail, name, lastname },
   });
