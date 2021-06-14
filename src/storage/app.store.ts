15538421771c0d26fe5a95a0e67dfd820c026a89
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2021-06-13 21:45:24 -0300

    redux persistance

--- src/storage/app.store.ts
+++ src/storage/app.store.ts
@@ -1,22 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { ConversationSlice } from "./conversation.reducer";
 import { CoreSlice } from "./core.reducer";
 import { FeedSlice } from "./feed.reducer";
 import { RegisterSlice } from "./register.reducer";
 import { SignInSlice } from "./signin.reducer";
 
-const store = configureStore({
-  reducer: {
-    signIn: SignInSlice.reducer,
-    register: RegisterSlice.reducer,
-    core: CoreSlice.reducer,
-    feed: FeedSlice.reducer,
-    conversation: ConversationSlice.reducer,
-  },
+const rootReducer = combineReducers({
+  signIn: SignInSlice.reducer,
+  register: RegisterSlice.reducer,
+  core: CoreSlice.reducer,
+  feed: FeedSlice.reducer,
+  conversation: ConversationSlice.reducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
 });
 
 export type AppState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+export const persistor = persistStore(store);
--- src/storage/conversation.reducer.ts
+++ src/storage/conversation.reducer.ts
@@ -4,12 +4,14 @@ export const fetchAll = createAsyncThunk("conversation/fetchAll", async () => {
   return [
     {
       id: 1,
+      authorId: "123",
       username: "PabloRocks123",
       firstName: "Pablo",
       lastMessage: "Yo when are we leaving?",
     },
     {
       id: 2,
+      authorId: "858585",
       username: "BigManlo",
       firstName: "Biggie",
       lastMessage: "BTC is going to the moon, you know it, i know it.",
@@ -21,23 +23,27 @@ export const ConversationSlice = createSlice({
   name: "conversation",
   initialState: {
     conversations: [],
-    loading: false,
-    loadingSuccess: true,
+    conversationsRequestStatus: {
+      loading: false,
+      loadingSuccess: true,
+      error: false,
+    },
   },
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchAll.fulfilled, (state, action) => {
         state.conversations = action.payload;
-        state.loading = false;
-        state.loadingSuccess = true;
+        state.conversationsRequestStatus.loading = false;
+        state.conversationsRequestStatus.loadingSuccess = true;
       })
       .addCase(fetchAll.pending, (state, action) => {
-        state.loading = true;
+        state.conversationsRequestStatus.loading = true;
       })
       .addCase(fetchAll.rejected, (state, action) => {
-        state.loading = false;
-        state.loadingSuccess = true;
+        state.conversationsRequestStatus.loading = false;
+        state.conversationsRequestStatus.loadingSuccess = true;
+        state.conversationsRequestStatus.error = true;
       });
   },
 });
