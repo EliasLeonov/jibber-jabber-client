8aeaa0618cd232672e71430e61dec02af84da31b
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2021-07-07 21:40:16 -0300

    https both

--- src/screens/chats/private.chat.screen.tsx
+++ src/screens/chats/private.chat.screen.tsx
@@ -179,7 +179,7 @@ const PrivateChatScreen = () => {
       </Container>
       {profile && myProfile && (
         <SocketJsClient
-          url={`http://${process.env.REACT_APP_URL}/ws`}
+          url={`https://${process.env.REACT_APP_URL}/ws`}
           topics={[`/user/${myProfile.id}/queue/messages`]}
           ref={(c) => {
             client = c;
